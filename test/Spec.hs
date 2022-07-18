bfff643f4a89933cfa5771b2139a712862075e97
Pierre Abbat <phma@bezitopo.org>
2022-07-18 09:00:03 +0000

    Add a unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE InstanceSigs #-}
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+import Cryptography.RandomLadder
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [qcProps]
+
+qcProps = testGroup "(checked by QuickCheck)"
+  [
+  ]
+
+unitTests = testGroup "Unit tests"
+  [ testCase "5882353" $
+      (randomLadder 1 (+) 0 5882353 8388608 16777216) @?= 5882353
+  ]
