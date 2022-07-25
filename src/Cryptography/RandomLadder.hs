ea726c9b24ac975f4ef3032e2103cbfde01c4be9
Pierre Abbat <phma@bezitopo.org>
2022-07-25 05:41:55 +0000

    Express randomLadder as two steps

--- src/Cryptography/RandomLadder.hs
+++ src/Cryptography/RandomLadder.hs
@@ -1,8 +1,6 @@
 module Cryptography.RandomLadder
     ( someFunc
     , bitize
-    , makeLadder
-    , climbLadder
     , randomLadder
     ) where
 import qualified Data.Sequence as Seq
@@ -66,25 +64,5 @@ randomLadder :: a -> (a -> a -> a) -> a -> Integer -> Integer -> Integer -> a
 -- range should be at least n**0.78788506
 -- random should be in [0..range)
 -- n should be nonnegative
-randomLadder _ _ _ (-1) _ _ = error "negative multiplier/exponent"
-randomLadder _ _ zero 0 _ _ = zero
-randomLadder gen _ _ 1 _ _ = gen
-randomLadder gen (<+>) _ 2 _ _ = gen <+> gen
 randomLadder gen (<+>) zero n random range =
-  let crit = (range * 373 + 322) `div` 644
-      (quo2,rem2) = n `divMod` 2
-      (quo3,rem3) = n `divMod` 3
-  in if random >= crit
-    then let third = randomLadder gen (<+>) zero quo3 (random - crit) (range - crit)
-    in
-      if rem3 == 0
-      then third <+> third <+> third
-      else
-	if rem3 == 1
-	then (third <+> third <+> third) <+> gen
-	else (third <+> third <+> third) <+> (gen <+> gen)
-    else let half = randomLadder gen (<+>) zero quo2 random crit
-    in
-      if rem2 == 0
-      then half <+> half
-      else (half <+> half) <+> gen
+  climbLadder (makeLadder n random range) gen (gen <+> gen) (<+>) zero
