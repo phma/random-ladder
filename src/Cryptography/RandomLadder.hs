3ee56f30a5661f5e47ff9aae0fb33e8dfdf319b5
Pierre Abbat <phma@bezitopo.org>
2022-07-25 04:57:59 +0000

    Write makeLadder, splitting randomLadder in two steps

--- src/Cryptography/RandomLadder.hs
+++ src/Cryptography/RandomLadder.hs
@@ -1,8 +1,11 @@
 module Cryptography.RandomLadder
     ( someFunc
     , bitize
+    , makeLadder
     , randomLadder
     ) where
+import qualified Data.Sequence as Seq
+import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)))
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -15,6 +18,34 @@ bitize random range =
   in if random >= crit
 	then '3' : bitize (random - crit) (range - crit)
 	else '2' : bitize random crit
+
+makeLadder :: Integer -> Integer -> Integer -> Seq.Seq Int
+-- makeLadder n random range -> sequence-of-instructions
+-- range should be at least n**0.78788506
+-- random should be in [0..range)
+-- n should be nonnegative
+makeLadder (-1) _ _ = error "negative multiplier/exponent"
+makeLadder 0 _ _ = Seq.empty
+makeLadder 1 _ _ = Seq.singleton 21
+makeLadder 2 _ _ = Seq.singleton 32
+makeLadder n random range =
+  let crit = (range * 373 + 322) `div` 644
+      (quo2,rem2) = n `divMod` 2
+      (quo3,rem3) = n `divMod` 3
+  in if random >= crit
+    then let third = makeLadder quo3 (random - crit) (range - crit)
+    in
+      if rem3 == 0
+      then 30 <| third
+      else
+	if rem3 == 1
+	then 31 <| third
+	else 32 <| third
+    else let half = makeLadder quo2 random crit
+    in
+      if rem2 == 0
+      then 20 <| half
+      else 21 <| half
 
 randomLadder :: a -> (a -> a -> a) -> a -> Integer -> Integer -> Integer -> a
 -- randomLadder gen (<+>) zero n random range -> n*gen
